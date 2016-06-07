e5227e6a3f48c4b0692d048bff7559a843089606
zhou <2273922304@qq.com>
2016-06-07 16:45:52 +0800

    qqqqq

---
+++ Git/Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 周灿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
